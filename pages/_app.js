96d6e786593359854d3aac73e697b3f807db8275
Simon <simon130699@gmail.com>
2023-01-09 08:28:14 -0300

    Navbar mobile

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import { NavBar } from "../components/Nav/NavBar"
 import "../styles/index.scss"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <NavBar/>
+    <Component {...pageProps} />
+    </>
+  )
 }
