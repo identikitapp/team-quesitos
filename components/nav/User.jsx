aab20a0682aa19c9929461b34a60838c935d49c8
Simon <simon130699@gmail.com>
2023-01-13 11:48:24 -0300

    add comments

---
+++ components/nav/User.jsx
@@ -0,0 +1,24 @@
+import logo from '../../public/title.png'
+import Image from 'next/image'
+import logoUser from '../../public/user2.png'
+import Link from 'next/link'
+const userName = {
+name: 'Adakos'
+}
+
+const User = () => {
+  return (
+    <div className='user'>
+    <div className='logo'>
+        <Link href={'/'}> <Image width={150} height={25} src={logo} /></Link>
+    </div>
+  <div className='name'>
+    <h3>{userName.name}</h3>
+    <Image className='logoUser' width={28.86} height={28.43} src={logoUser} />
+
+  </div>
+    </div>
+  )
+}
+
+export default User
--- pages/perfil/index.jsx
+++ pages/perfil/index.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react"
+import Coments from "../../components/coments/Coments"
+import User from "../../components/nav/User"
 
 const Profile = () => {
 
-    const [publications, setPublications] = useState([dataTest1, dataTest1, dataTest1, dataTest1])
+    // const [publications, setPublications] = useState([dataTest1, dataTest1, dataTest1, dataTest1])
     const [seePost, setSeePost] = useState([null, null])
     const [seeImage, setSeeImage] = useState(false)
 
@@ -14,7 +16,10 @@ const Profile = () => {
 
     return (
         <div className="profileContainer">
-
+            <User/>
+         <Coments/>
+        <Coments/>
+        <Coments/>
         </div>
     )
 }
