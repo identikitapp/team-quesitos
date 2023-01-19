06e6ac1930d53a4134958d3854d93d1396b8a6ee
schulz-franco <fschulz2222@gmail.com>
2023-01-18 22:28:47 -0300

    [feature] UI feed mobile

--- pages/feed/index.jsx
+++ pages/feed/index.jsx
@@ -1,24 +1,44 @@
 import Nav from "../../components/nav/Nav"
-import { useUserContext } from "../../context/user"
+import NewPost from "../../components/post/NewPost"
+import Post from "../../components/post/Post"
 import testImg1 from "../../public/testImg1.png"
 
 let dataTest1 = {
-    username: "adakoss",
-    image: testImg1,
-    date: "28 de diciembre",
-    content: "texto de prueba texto de prueba texto de texto de prueba texto de prueba texto de prueba texto de prueba texto de",
-    likes: 0,
-    comments: []
+    username: "adakos",
+    name: null,
+    lastname: null,
+    profile: "https://m.media-amazon.com/images/I/712Q60481-L._CR0,204,1224,1224_UX256.jpg",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus iure, quos dolorem eligendi totam velit ullam. Voluptate eos.",
+    image: null
+}
+
+let dataTest2 = {
+    username: "adakos",
+    name: "franco",
+    lastname: "schulz",
+    profile: "https://m.media-amazon.com/images/I/712Q60481-L._CR0,204,1224,1224_UX256.jpg",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus iure, quos dolorem eligendi totam velit ullam. Voluptate eos.",
+    image: null
+}
+
+let dataTest3 = {
+    username: "adakos",
+    name: "franco",
+    lastname: "schulz",
+    profile: "https://m.media-amazon.com/images/I/712Q60481-L._CR0,204,1224,1224_UX256.jpg",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus iure, quos dolorem eligendi totam velit ullam. Voluptate eos.",
+    image: testImg1
 }
 
 const Feed = () => {
 
-    const { user } = useUserContext()
-
     return (
         <div className="feedContainer">
             <Nav/>
-            <span>{user.username}</span>
+            <NewPost />
+            <Post data={dataTest1} />
+            <Post data={dataTest2} />
+            <Post data={dataTest3} />
         </div>
     )
 }
