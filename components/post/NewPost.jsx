7b0959c8b5fcfacecddb92490a42c6644f648e25
schulz-franco <fschulz2222@gmail.com>
2023-02-01 00:08:52 -0300

    UI del formulario para crear un post

--- components/post/NewPost.jsx
+++ components/post/NewPost.jsx
@@ -2,23 +2,30 @@ import { useRef } from "react"
 import useNewPost from "../../hooks/useNewPost"
 import Loader from "../Loader"
 import Image from "next/image"
-import uploadImg from "../../public/post/upload.png"
+import uploadImg from "../../public/post/upload.svg"
+import profileImg from "../../public/profile.png"
+import { useUserContext } from "../../context/user"
 
 const NewPost = ({ update, setUpdate }) => {
 	
+	const { user } = useUserContext()
 	const { error, loader, length, setLength, onSubmitHandler } = useNewPost(update, setUpdate)
 	const inputFileRef = useRef()
 
-	let buttonValue = loader ? <Loader width={18} height={18} /> : "Publicar"
+	const buttonValue = loader ? <Loader width={18} height={18} /> : "Publicar"
+	const userImage = user.image ? user.image : profileImg
 
 	return (
 		<form onSubmit={(ev)=> onSubmitHandler(ev)} className="newPostForm">
 			{error.error && <span className="error">{error.message}</span>}
 			<input ref={inputFileRef} style={{display: "none"}} type="file" name="image" accept="image/*" />
-			<textarea onChange={(ev)=> setLength(ev.target.value.length)} name="content" cols="120" rows="6" maxLength={200} placeholder="¿En qué estas pensando?"/>
-			<div>
+			<div className="textarea">
+				<textarea onChange={(ev)=> setLength(ev.target.value.length)} name="content" cols="120" rows="6" maxLength={200} placeholder="¿En qué estas pensando?"/>
+				<Image width={50} height={50} src={userImage} alt={user.username}></Image>
+			</div>
+			<div className="buttons">
 				<span>{length}/200</span>
-				<Image onClick={()=> inputFileRef.current.click()} width='26' height='26' src={uploadImg} alt='Subir image' />
+				<Image onClick={()=> inputFileRef.current.click()} width='30' height='30' src={uploadImg} alt='Subir image' />
 				<button type="submit">{buttonValue}</button>
 			</div>
 		</form>
